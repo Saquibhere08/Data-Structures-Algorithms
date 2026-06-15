d1fed7591bde1f601107a89c511ae46e19b55bed
Saquib Bin Halim <halimsaquib99@gmail.com>
2026-06-15 12:46:57 +0530

    Add files via upload

---
+++ arrays/SumofArrays.java
@@ -0,0 +1,13 @@
+package arrays;
+
+public class SumofArrays {
+    public static void main(String[] args) {
+        int sum=0;
+        int arr[] ={10,3,5,9,21,1}; //array 
+
+        for(int i=0;i<arr.length;i++){
+            sum+=arr[i]; //sum of Arrays
+        }
+        System.out.println("Sum of Elements: "+sum); //49
+    }
+}
